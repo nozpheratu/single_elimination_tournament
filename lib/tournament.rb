152c08afb9745d35a484d47d9b1c925e88250683
robot <cylehunter33@gmail.com>
2017-02-21 14:10:56 -0600

    Cleaned up tournament class

--- lib/tournament.rb
+++ lib/tournament.rb
@@ -1,9 +1,22 @@
+require 'csv'
 require_relative 'tournament/bracket'
 require_relative 'tournament/input'
 require_relative 'tournament/graph'
-require 'csv'
-require 'ap'
 
+# The program takes a path to a CSV containing a line-delimited list of users to
+# be used as players for a single elimitation style tournament. The program will
+# automatically setup the tournament bracket and the user will be prompted to
+# select the outcome for each successive match. When all match outcomes have
+# been decided, a tournament bracket/tree digram will be generated depicting the
+# results of the tournament.
+#
+# Author::    Dave Thomas  (mailto:dave@x.y)
+# Copyright:: Copyright (c) 2002 The Pragmatic Programmers, LLC
+# License::   Distributes under the same terms as Ruby
+
+# This class accepts a path to a CSV, parses the CSV into a collection of player
+# names and then passes the player names down the pipeline for bracket
+# generation, outcome specification, and finally graph generation.
 module Tournament
   class << self
     def run(path)
@@ -17,7 +30,7 @@ module Tournament
 
     def parse_names(path)
       @file = File.join(path)
-      names = Array.new
+      names = []
       CSV.foreach(@file) do |field|
         names << field[0]
       end

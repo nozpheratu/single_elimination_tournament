b1227e6440f1ee6e2d0e65360f77f77b67cc7c68
robot <cylehunter33@gmail.com>
2017-02-21 15:04:26 -0600

    Cleanup

--- lib/tournament/graph.rb
+++ lib/tournament/graph.rb
@@ -2,6 +2,9 @@ require 'graph'
 require 'fileutils'
 
 module Tournament
+  # The Graph class is responsible for generating application output in the form
+  # of a SVG + PNG diagram depicting the tournament bracket. It utilizes the
+  # graph ruby DSL for Graphviz to acomplish this.
   class Graph
     def initialize(bracket_tree)
       @bracket_tree = bracket_tree
@@ -13,9 +16,9 @@ module Tournament
     def generate
       matches = @bracket_tree
       path = "./graph"
-      FileUtils.mkdir(path) unless File.exists? path
-      final = @bracket_tree.sort {|match| match[:round]}.first
-      digraph do |x, &block|
+      FileUtils.mkdir(path) unless File.exist? path
+      final = @bracket_tree.sort { |match| match[:round] }.first
+      digraph do
         boxes
         label("Single Elimination Tournament")
         orient "LR"
@@ -27,7 +30,9 @@ module Tournament
 
         # Node & edge setup
         matches.each do |match|
-          match_edge = matches.find {|e| e[:round] == match[:round] + 1 && [e[:home_id], e[:away_id]].include?(match[:winner_id])}
+          match_edge = matches.find do |e|
+            e[:round] == match[:round] + 1 && [e[:home_id], e[:away_id]].include?(match[:winner_id])
+          end
           node(match.object_id).label "#{match[:home_id]}\nvs.\n#{match[:away_id]}"
           edge match.object_id, match_edge.object_id unless match_edge.nil?
         end
@@ -37,7 +42,8 @@ module Tournament
         edge final.object_id, "winner"
 
         # Assigns up to 8 rounds a unique colour
-        matches.sort {|match| match[:round]}.reverse.group_by{|match| match[:round]}.map do |round, round_matches|
+        sorted_matches = matches.sort { |match| match[:round] }.reverse
+        sorted_matches.group_by { |match| match[:round] }.map do |_, round_matches|
           colour += 1 unless colour >= 8
           round_matches.each do |match|
             unless match[:winner_id].nil?
@@ -46,7 +52,6 @@ module Tournament
           end
           eval("c#{colour + 1}") << node("winner")
         end
-        #save as svg and fallback png
         save "#{path}/graph", "svg"
         save "#{path}/graph", "png"
       end

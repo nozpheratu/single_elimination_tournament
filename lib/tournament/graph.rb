f3c9cacd4df39ef6a5476f20111f7252e22bbdc3
robot <cylehunter33@gmail.com>
2017-02-22 18:37:22 -0600

    Cleanup

--- lib/tournament/graph.rb
+++ lib/tournament/graph.rb
@@ -1,48 +1,32 @@
 require 'graph'
-require 'fileutils'
 
 module Tournament
   # The Graph class is responsible for generating application output in the form
   # of a SVG + PNG diagram depicting the tournament bracket. It utilizes the
   # graph ruby DSL for Graphviz to acomplish this.
   class Graph
-    def initialize(bracket_tree)
-      @bracket_tree = bracket_tree
-      generate if @bracket_tree.any?
+    attr_reader :bracket
+
+    def initialize(bracket)
+      @bracket = bracket
+      generate if @bracket.any?
     end
 
     private
 
     def generate
-      matches = @bracket_tree
-      path = "./graph"
-      FileUtils.mkdir(path) unless File.exist? path
-      final = @bracket_tree.sort { |match| match[:round] }.first
+      klass = self # Accommodate the context change in digraph hash
+      final = @bracket.sort { |match| match[:round] }.first
       digraph do
-        boxes
+        # General graph configuration
         label("Single Elimination Tournament")
-        orient "LR"
-        colour = 0
-        colorscheme(:pubu, 8)
+        boxes; orient "LR"; colour = 0; colorscheme(:pubu, 8)
         graph_attribs << "labelloc=\"t\""
         edge_attribs << ["arrowhead=none", "arrowtail=none", "style=bold"]
         node_attribs << filled
 
-        # Node & edge setup
-        matches.each do |match|
-          match_edge = matches.find do |e|
-            e[:round] == match[:round] + 1 && [e[:home], e[:away]].include?(match[:winner])
-          end
-          node(match.object_id).label "#{match[:home]}\nvs.\n#{match[:away]}"
-          edge match.object_id, match_edge.object_id unless match_edge.nil?
-        end
-
-        node("winner").label final[:winner]
-
-        edge final.object_id, "winner"
-
         # Assigns up to 8 rounds a unique colour
-        sorted_matches = matches.sort { |match| match[:round] }.reverse
+        sorted_matches = klass.bracket.sort { |match| match[:round] }.reverse
         sorted_matches.group_by { |match| match[:round] }.map do |_, round_matches|
           colour += 1 unless colour >= 8
           round_matches.each do |match|
@@ -52,8 +36,22 @@ module Tournament
           end
           eval("c#{colour + 1}") << node("winner")
         end
-        save "#{path}/graph", "svg"
-        save "#{path}/graph", "png"
+
+        # Node & edge setup
+        klass.bracket.each do |match|
+          match_edge = klass.bracket.find do |e|
+            e[:round] == match[:round] + 1 && [e[:home], e[:away]].include?(match[:winner])
+          end
+          node(match.object_id).label "#{match[:home]}\nvs.\n#{match[:away]}"
+          edge match.object_id, match_edge.object_id unless match_edge.nil?
+        end
+
+        # Setup the final winner node
+        node("winner").label final[:winner]
+        edge final.object_id, "winner"
+
+        save "./graph/graph", "svg"
+        save "./graph/graph", "png"
       end
     end
   end
